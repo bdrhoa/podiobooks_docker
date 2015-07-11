74ba7003095db46bb119b5cd7d7a0dd206b64ad8
cyface <tim@cyface.com>
2015-07-11 17:47:28 -0600

    working with locmem...redis not working

--- podiobooks-web/irun.sh
+++ podiobooks-web/irun.sh
@@ -1,5 +1,5 @@
 sudo docker run --rm -it \
-	-p 127.0.0.1:60080:80 \
 	--volumes-from podiobooks-data \
 	--link podiobooks-db:db \
+	--link podiobooks-redis:redis \
 	podiobooks/podiobooks-web /bin/bash
