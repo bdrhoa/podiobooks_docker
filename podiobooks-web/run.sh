19cc2c21841d59cd2ce030b0237146d828ca6835
Tim White <tim@cyface.com>
2015-10-29 10:07:56 -0400

    fixing virtualenv option for uwsgi

--- podiobooks-web/run.sh
+++ podiobooks-web/run.sh
@@ -1,5 +1,5 @@
 sudo docker run -d --name="podiobooks-web" \
-	-p 127.0.0.1:60080:80 \
+	-p 127.0.0.1:80:80 \
 	--link podiobooks-db:db \
 	--link podiobooks-redis:redis \
 	--volumes-from podiobooks-data \
