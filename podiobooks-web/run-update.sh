2f30ad6c9c356a68c815654670ae06d3b00b69f1
Tim White <tim@cyface.com>
2015-10-30 12:44:13 -0400

    added restart to update script

--- podiobooks-web/run-update.sh
+++ podiobooks-web/run-update.sh
@@ -1,5 +1,5 @@
 sudo docker run --rm -it \
         --volumes-from podiobooks-data \
         podiobooks/podiobooks-web /opt/podiobooks/data/podiobooks/devscripts/docker/podiobooks_update.sh
-#./stop.sh
-#./start.sh
+./stop.sh
+./start.sh
