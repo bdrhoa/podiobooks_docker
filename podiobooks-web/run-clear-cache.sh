92330837003cff280984f0f279b6711498206af1
Tim White <tim@cyface.com>
2015-11-01 19:11:06 -0500

    Added clear cache script

---
+++ podiobooks-web/run-clear-cache.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sudo docker run --rm -it \
+        --volumes-from podiobooks-data \
+	    --link podiobooks-db:db \
+        --link podiobooks-redis:redis \
+        podiobooks/podiobooks-web /opt/podiobooks/data/podiobooks/devscripts/docker/podiobooks_clear_cache.sh
