dc5900b577542a0d3ec07b831e6e1ce154e254ee
Tim White <tim@cyface.com>
2015-11-01 19:34:46 -0500

    adding db to update script

--- podiobooks-web/run-update.sh
+++ podiobooks-web/run-update.sh
@@ -1,5 +1,7 @@
 sudo docker run --rm -it \
         --volumes-from podiobooks-data \
+        --link podiobooks-db:db \
+        --link podiobooks-redis:redis \
         podiobooks/podiobooks-web /opt/podiobooks/data/podiobooks/devscripts/docker/podiobooks_update.sh
 ./stop.sh
 ./start.sh
