4b8c59c77788c2200a316a3412539bbd6bd4c57b
Tim White <tim@cyface.com>
2015-11-10 17:07:21 -0500

    updating scripts

--- stop-all.sh
+++ stop-all.sh
@@ -2,7 +2,7 @@
 # This file stops all the containers
 ./podiobooks-db/stop.sh # Stop up the Postgres container
 ./podiobooks-redis/stop.sh # Stop the Redis container
-./podiobooks-varnish/stop.sh # Start up the Varnish container
+./podiobooks-varnish/stop.sh # Stop up the Varnish container
 ./podiobooks-web/stop.sh # Stop the Web Layer
 
 
