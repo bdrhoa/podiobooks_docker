44f667475df51a1ea6b6d8bb90b2659ea6f682d1
Tim White <tim@cyface.com>
2015-11-11 12:21:07 -0700

    Adding non-docker-megascript.

--- non-docker-megascript.sh
+++ non-docker-megascript.sh
@@ -2,7 +2,9 @@
 # This file runs everything to set up a single server, rather than individual Docker containers.
 # This assumes CentOS 7.1 as the base
 
-# Start with this: git clone git@github.com:podiobooks/podiobooks_docker.git
+# Start with this:
+# yum install -y git
+# git clone https://github.com/podiobooks/podiobooks_docker.git
 
 ## OS
 
